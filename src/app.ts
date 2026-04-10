ed17080c6a8d235654e1f220d221a653f06fcce2
gg1700 <gabrielcamacho1700@gmail.com>
2026-04-10 19:20:48 -0400

    refactor: update database migration and population scripts to check for public schema tables; rename register route file

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import "./config/env.config";
 import app from "./config/server.config";   
 import { env } from "./config/env.config";
+import { getConextionString } from "./config/database.config";
 
 app.listen(env.PORT, () => {
   console.log(`Servidor escuchando en el Puerto: ${env.PORT}`);
   console.log(`Servidor funcionando en: http://localhost:${env.PORT}/health`);
+  console.log(`Servidor conectado a la base de datos: ${getConextionString()}`);
 });
--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,7 +1,18 @@
 import { Pool } from "pg";
 import * as dotenv from "dotenv";
+import * as path from "path";
 
-dotenv.config();
+dotenv.config({ path: path.join(process.cwd(), ".env") });
+
+//console.log("--- DEBUG DE CONEXIÓN ---");
+//console.log("Entorno detectado:", process.env.NODE_ENV);
+//console.log("URL de BD detectada:", process.env.NODE_ENV === "production" ? process.env.DB_URL : process.env.LOCAL_DB_URL);
+//console.log("-------------------------");
+
+export function getConextionString() {
+  const connectionString = process.env.NODE_ENV === "production" ? process.env.DB_URL : process.env.LOCAL_DB_URL;
+  return connectionString;
+}
 
 const pool = new Pool({
   connectionString: process.env.NODE_ENV === "production" ? process.env.DB_URL : process.env.LOCAL_DB_URL,
