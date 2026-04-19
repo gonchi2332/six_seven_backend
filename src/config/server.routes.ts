e82204805be28b3d298496ef2a49d9d232bffbb7
gg1700 <gabrielcamacho1700@gmail.com>
2026-04-18 22:50:31 -0400

    feat: update API version in routing for authentication, registration, and verification

--- src/config/server.routes.ts
+++ src/config/server.routes.ts
@@ -12,9 +12,9 @@ router.get("/health", (req: Request, res: Response) => {
   });
 });
 
-router.use("/api/v1/auth", AuthRoutes);
-router.use("/api/v1/register", RegisterRoutes);
-router.use("/api/v1/verification", VerificationRoutes);
+router.use("/api/v2/auth", AuthRoutes);
+router.use("/api/v2/register", RegisterRoutes);
+router.use("/api/v2/verification", VerificationRoutes);
 
 router.use((req: Request, res: Response) => {
   res.status(404).json({ error: "Ruta no encontrada..." });
