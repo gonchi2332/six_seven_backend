56c05c947b3fe8ea2432b153e81b031a9dc57692
Leonel Zeballos Aldunate <zeballos.leonel@outlook.com>
2026-04-25 10:14:58 -0400

    feat: add new endpoints for view, register, modify and delete soft skills

--- src/controllers/skill.controller.ts
+++ src/controllers/skill.controller.ts
@@ -181,9 +181,55 @@ export async function deleteHardSkill(req: Request, res: Response) {
   }
 }
 
-export async function viewSoftSkills(req: Request, res: Response) {
+export async function registerSoftSkill(req: Request, res: Response) {
   try {
     const { username } = req.user as TokenTypes.TokenPayload;
+    let { skillName } = req.body;
+
+    if (!skillName || typeof skillName !== "string" || skillName.trim().length === 0) {
+      return res.status(400).json({
+        success: false,
+        message: "El campo nombre de la habilidad es obligatorio."
+      });
+    }
+    
+    skillName = skillName.trim();
+    if (skillName.length > 50) {
+      return res.status(400).json({
+        success: false,
+        message: "El máximo es 50 caracteres"
+      });
+    }
+
+    if (!latinAlphabetRegex.test(skillName)) {
+      return res.status(400).json({
+        success: false,
+        message: "Solo se permite caracteres del alfabeto latino"
+      });
+    }
+
+    const { result, messageState } = await SkillService.registerUserSoftSkill(username, skillName.trim());
+    if (!result) {
+      return res.status(400).json({
+        success: false,
+        message: messageState
+      });
+    }
+    return res.status(201).json({
+      success: true,
+      message: messageState
+    });
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      message: `Error en el servidor: ${(err as Error).message}`
+    });
+  }
+}
+
+export async function viewSoftSkills(req: Request, res: Response) {
+  try {
+    const { username } = req.params;
 
     if (!username || typeof username !== "string") {
       return res.status(400).json({
@@ -210,42 +256,82 @@ export async function viewSoftSkills(req: Request, res: Response) {
   }
 }
 
-export async function registerSoftSkill(req: Request, res: Response) {
+export async function modifySoftSkill(req: Request, res: Response) {
   try {
     const { username } = req.user as TokenTypes.TokenPayload;
-    let { skillName } = req.body;
+    const oldSkillName = req.body.oldSkillName;
+    let newSkillName = req.body.newSkillName;
 
-    if (!skillName || typeof skillName !== "string" || skillName.trim().length === 0) {
+    if (!oldSkillName || typeof oldSkillName !== "string" || !newSkillName || typeof newSkillName !== "string") {
       return res.status(400).json({
-        success: false, message: "El campo nombre de la habilidad es obligatorio."
+        success: false,
+        message: "Datos incompletos o inválidos."
+      });
+    }
+
+    newSkillName = newSkillName.trim();
+
+    if (newSkillName.length > 50) {
+      return res.status(400).json({
+        success: false,
+        message: "El máximo es 50 caracteres"
+      });
+    }
+
+    if (!latinAlphabetRegex.test(newSkillName)) {
+      return res.status(400).json({
+        success: false,
+        message: "Solo se permite caracteres del alfabeto latino"
+      });
+    }
+
+    const { result, messageState } = await SkillService.modifyUserSoftSkill(username, oldSkillName.trim(), newSkillName);
+    if (!result) {
+      return res.status(400).json({
+        success: false,
+        message: messageState
       });
     }
     
-    skillName = skillName.trim();
-    if (skillName.length > 50) {
-      return res.status(400).json({
-        success: false, message: "El máximo es 50 caracteres"
-      });
-    }
-
-    if (!latinAlphabetRegex.test(skillName)) {
-      return res.status(400).json({
-        success: false, message: "Solo se permite caracteres del alfabeto latino"
-      });
-    }
-
-    const { result, messageState } = await SkillService.registerUserSoftSkill(username, skillName.trim());
-    if (!result) {
-      return res.status(400).json({
-        success: false, message: messageState
-      });
-    }
-    return res.status(201).json({
-      success: true, message: messageState
+    return res.status(200).json({
+      success: true,
+      message: "Habilidad modificada correctamente"
     });
   } catch (err) {
     return res.status(500).json({
-      success: false, message: `Error en el servidor: ${(err as Error).message}`
+      success: false,
+      message: `Error en el servidor ${(err as Error).message}`
+    });
+  }
+}
+
+export async function deleteSoftSkill(req: Request, res: Response) {
+  try {
+    const { username } = req.user as TokenTypes.TokenPayload;
+    const { skillName } = req.body;
+
+    if (!skillName || typeof skillName !== "string") {
+      return res.status(400).json({
+        success: false,
+        message: "Nombre de habilidad incompleto o inválido."
+      });
+    }
+
+    const { result, messageState } = await SkillService.deleteUserSoftSkill(username, skillName.trim());
+    if (!result) {
+      return res.status(400).json({
+        success: false,
+        message: messageState
+      });
+    }
+    return res.status(200).json({
+      success: true,
+      message: "Habilidad blanda eliminada correctamente."
+    }); 
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      message: `Error en el servidor ${(err as Error).message}`
     });
   }
 }
