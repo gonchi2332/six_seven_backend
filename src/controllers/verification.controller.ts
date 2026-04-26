1910da84c14be0381eea2c0e15d73fa23129e918
Israel Orosco <israel-orosco@hotmail.com>
2026-04-25 20:46:26 -0400

    Feat: added endpoint to get the user email.

--- src/controllers/verification.controller.ts
+++ src/controllers/verification.controller.ts
@@ -2,6 +2,37 @@ import { Request, Response } from "express";
 import * as TokenTypes from "../types/token.types";
 import * as VerificationService from "../services/verification.service";
 
+export async function getUserMail(req: Request, res: Response) {
+  try {
+    const { username } = req.user as TokenTypes.TokenPayload;
+  
+    if (!username || typeof username !== "string") {
+      return res.status(400).json({
+        success: false,
+        message: "Nombre de usuario invalido."
+      });
+    }
+
+    const { result, messageState, email } = await VerificationService.getUserMail(username);
+    if (!result) {
+      return res.status(400).json({
+        success: false,
+        message: messageState
+      });
+    }
+    return res.status(200).json({
+      success: true,
+      message: messageState,
+      email: email
+    });
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      messageState: `Error interno del servidor: ${(err as Error).message}`
+    });
+  }
+}
+
 export async function sendMailVerification(req: Request, res: Response) {
   try {
     const { username } = req.user as TokenTypes.TokenPayload;
--- src/routes/verification.routes.ts
+++ src/routes/verification.routes.ts
@@ -3,7 +3,11 @@ import * as Authorization from "../middlewares/authorization.middleware";
 import * as VerificationController from "../controllers/verification.controller";
 
 const router = Router();
-
+router.get(
+  "/users/mail",
+  Authorization.tokenAuthorization,
+  VerificationController.getUserMail
+);
 router.post(
   "/users/verification-code",
   Authorization.tokenAuthorization,
